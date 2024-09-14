f7b111bdf4131be8d5143bdeee56bd0fe05bc7e3
shreeshams123 <[shreesha2002ms@gmail.com]>
2024-09-14 11:49:09 +0530

    Added project

---
+++ EncapsulateExample/Demo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EncapsulateExample
+{
+    public class Demo
+    {
+        private string location;
+        public Demo(string name)
+        {
+            Name = name;
+        }
+        public string Name
+        {
+            get => location;
+            set => location = value;
+        }
+    }
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            Demo d = new Demo("Bangalore");
+            Console.WriteLine(d.Name);
+        }
+    }
+}
---
+++ EncapsulateExample/University.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncapsulateExample
+{
+    public class University
+    {
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name");
+            University u = new University();
+            u.Name = Console.ReadLine();
+            Console.WriteLine(u.Name);
+
+
+        }
+
+    }
+}
